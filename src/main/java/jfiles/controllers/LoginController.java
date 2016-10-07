2efa52a7e9ffd6774e2480622195c93a474cecb2
YOUR NAME <andrew_yudaichev@i.ua>
2016-10-07 15:09:55 +0300

    Project setup for transfer to GAE. First sample which work with Spring and Hibernate

---
+++ src/main/java/jfiles/controllers/LoginController.java
@@ -0,0 +1,63 @@
+package jfiles.controllers;
+
+import jfiles.model.TableEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Controller
+public class LoginController {
+
+//    @Autowired
+//    private EntityManagerFactory emf;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @RequestMapping(value = "/")
+    public String index(){
+
+//        em = emf.createEntityManager();
+
+        return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginPagePost(Model model,
+                                @RequestParam String login,
+                                @RequestParam String password){
+
+        TableEntity te = hibernateDbAccess(login);
+
+        if( te == null)
+            return "loginJSP";
+
+        if( password.contentEquals( te.getPassword())){
+
+            model.addAttribute("authorized", true);
+            model.addAttribute("log"       , login + "x");
+            model.addAttribute("pass"      , password + "x");
+        } else {
+
+            model.addAttribute("authorized", false);
+        }
+
+        return "loginJSP";
+    }
+
+    private TableEntity hibernateDbAccess(String login){
+
+//        em.getTransaction().begin();
+
+        TableEntity res = em.find(TableEntity.class, login);
+
+//        em.getTransaction().commit();
+        return res;
+    }
+
+}
---
+++ src/main/java/jfiles/model/TableEntity.java
@@ -0,0 +1,32 @@
+package jfiles.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "testuserauth", schema = "xodatabase", catalog = "")
+public class TableEntity {
+
+    private String login;
+    private String password;
+
+    @Id
+    @Column(name="login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Basic
+    @Column(name="password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
